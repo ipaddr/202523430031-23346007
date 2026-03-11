46ede873ff24899c95c70d03f4d54863009e6f26
fajar <fajarpratama2803@gmail.com>
2026-03-12 04:39:57 +0700

    Preparing Notes View to Read All Notes

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 import '../services/auth/auth_service.dart';
 import '../constants/routes.dart';
+import '../services/crud/notes_service.dart';
+import '../services/crud/note.dart';
 
-class NotesView extends StatelessWidget {
+class NotesView extends StatefulWidget {
   const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+
+  final NotesService _notesService = NotesService();
+
+  @override
+  void initState() {
+    super.initState();
+    _notesService.getAllNotes();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main UI'),
+        title: const Text('Your Notes'),
         actions: [
           IconButton(
             onPressed: () async {
@@ -23,8 +39,58 @@ class NotesView extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(
-        child: Text('Hello World'),
+
+      body: StreamBuilder<List<DatabaseNote>>(
+        stream: _notesService.allNotes,
+        builder: (context, snapshot) {
+
+          switch (snapshot.connectionState) {
+
+            case ConnectionState.waiting:
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+
+            case ConnectionState.active:
+
+              if (snapshot.hasData) {
+
+                final notes = snapshot.data!;
+
+                if (notes.isEmpty) {
+                  return const Center(
+                    child: Text('No Notes Yet'),
+                  );
+                }
+
+                return ListView.builder(
+                  itemCount: notes.length,
+                  itemBuilder: (context, index) {
+
+                    final note = notes[index];
+
+                    return ListTile(
+                      title: Text(
+                        note.text,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    );
+                  },
+                );
+
+              } else {
+                return const Center(
+                  child: Text('No notes available'),
+                );
+              }
+
+            default:
+              return const Center(
+                child: Text('Something went wrong'),
+              );
+          }
+        },
       ),
     );
   }
